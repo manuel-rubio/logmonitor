5d6af9e92f9fb6b8f30d2945bd65f3f14ba6561e
Manuel Rubio <manuel@altenwald.com>
2018-06-30 04:11:38 +0200

    add parse for bad lines

--- src/github.com/manuel-rubio/logmonitor/logyzer/parse.go
+++ src/github.com/manuel-rubio/logmonitor/logyzer/parse.go
@@ -2,25 +2,52 @@ package logyzer
 
 import (
     "regexp"
-    "fmt"
     "strconv"
     "strings"
 )
 
 type LogEntry struct {
-    clientIP string;
-    proxyIPs []string;
-    datetime string;
-    method string;
-    uri string;
-    httpver string;
-    statuscode int;
-    responsetime int;
+    clientIP string
+    proxyIPs []string
+    datetime string
+    method string
+    uri string
+    httpver string
+    statuscode int
+    responsetime int
+    badLine bool
 }
 
-func Parse(line string) (LogEntry)  {
-    var re = regexp.MustCompile(`(?m)^(?P<ClientIP>[0-9]+(?:\.[0-9]+){3})(?P<ProxyIPs>(?:, [0-9]+(?:\.[0-9]+){3})*) (\[[^]]+\]) "([A-Z]+) ([^ ]+) ([^"]+)" ([0-9]{3}) ([0-9.]+)$`)
-    match := re.FindStringSubmatch(line)
+func (logentry *LogEntry) ProxyIPs() ([]string) {
+    return logentry.proxyIPs
+}
+
+func (logentry *LogEntry) NumProxyIPs() (int) {
+    return len(logentry.proxyIPs)
+}
+
+func (logentry *LogEntry) Method() (string) {
+    return logentry.method
+}
+
+func (logentry *LogEntry) IsBadLine() (bool) {
+    return logentry.badLine
+}
+
+func Parse(line string) LogEntry  {
+    reClientIP := `([0-9]+(?:\.[0-9]+){3})`
+    reProxyIPs := `((?:, [0-9]+(?:\.[0-9]+){3})*)`
+    reDatetime := `\[([^]]+)\]`
+    reQuery := `"([A-Z]+) ([^ ]+) ([^"]+)"`
+    reStatus := `([0-9]{3})`
+    reReqtime := `([0-9.]+)`
+    re := `(?m)^` + reClientIP + reProxyIPs + " " + reDatetime + " " +
+          reQuery + " " + reStatus + " " + reReqtime + "$"
+    var rg = regexp.MustCompile(re)
+    match := rg.FindStringSubmatch(line)
+    if match == nil {
+        return LogEntry{badLine: true}
+    }
     statuscode, _ := strconv.Atoi(match[7])
     responsetime, _ := strconv.ParseFloat(match[8], 64)
     logEntry := LogEntry{clientIP: match[1],
@@ -30,7 +57,8 @@ func Parse(line string) (LogEntry)  {
                          uri: match[5],
                          httpver: match[6],
                          statuscode: statuscode,
-                         responsetime: int(responsetime * 1000)}
+                         responsetime: int(responsetime * 1000),
+                         badLine: false}
     return logEntry
 }
 
