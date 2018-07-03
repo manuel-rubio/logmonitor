6128e12feae6c35944bb10630f1f19cc312b7748
Manuel Rubio <manuel@altenwald.com>
2018-07-04 01:54:02 +0200

    improve detection of inefficient proxy

--- src/github.com/manuel-rubio/logmonitor/logproxy/alert.go
+++ src/github.com/manuel-rubio/logmonitor/logproxy/alert.go
@@ -9,57 +9,92 @@ import(
 )
 
 type proxyChain []string
-
-func (pc proxyChain) equal(other proxyChain) (bool) {
-    for i, v := range pc {
-        if v != other[i] {
-            return false
-        }
-    }
-    return true
-}
-
 type proxyChains []proxyChain
-type efficientProxies map[string]proxyChains
 
-type inefficientProxies struct {
-    proxies []string
-    efficientChains proxyChains
+type proxyPath map[string]bool
+
+type proxy struct {
+    paths proxyPath
+    distance int
 }
 
-func (ip *inefficientProxies) add(proxy string, chains proxyChains) {
-    addProxy := false
-    for _, chain1 := range chains {
-        isEqual := false
-        for _, chain2 := range ip.efficientChains {
-            if chain1.equal(chain2) {
-                isEqual = true
-                break
+type efficientProxies map[string]proxy
+
+func (ep *efficientProxies) process(pc proxyChain, i int,
+                                    ie *inefficientProxies) (string, int) {
+    pname := pc[0]
+    var path proxyChain
+    if len(pc) > 1 {
+        path = pc[1:]
+    }
+    if p, ok := (*ep)[pname]; ok {
+        if len(path) > 0 {
+            n, d := ep.process(path, i + 1, ie)
+            if d < p.distance {
+                p.distance = d
+                p.paths = make(proxyPath)
+                p.paths[n] = true
+                (*ep)[pname] = p
+            } else if d > p.distance {
+                prefix := make(proxyChain, i)
+                copy(prefix, ie.originalChain[:i + 1])
+                ep.triggerAlert(prefix, pname, ie)
+                return pname, p.distance + 1
+            } else {
+                p.paths[n] = true
+            }
+            return pname, d + 1
+        } else {
+            if 0 < p.distance {
+                p.distance = 0
+                p.paths = make(proxyPath)
+                (*ep)[pname] = p
+            }
+            return pname, 1
+        }
+    } else {
+        d := 0
+        h := make(proxyPath)
+        if len(pc) > 1 {
+            var n string
+            n, d = ep.process(pc[1:], i + 1, ie)
+            h[n] = true
+        }
+        (*ep)[pname] = proxy{paths: h, distance: d}
+        return pname, d + 1
+    }
+}
+
+func (ep *efficientProxies) generateChains(prefix proxyChain, name string) (proxyChains) {
+    var chains proxyChains
+    prefix = append(prefix, name)
+    paths := (*ep)[name].paths
+    if len(paths) > 0 {
+        for proxy, _ := range paths {
+            if (*ep)[name].distance > 0 {
+                genChains := ep.generateChains(prefix, proxy)
+                chains = append(chains, genChains...)
+            } else {
+                chains = append(chains, prefix)
             }
         }
-        if !isEqual {
-            ip.efficientChains = append(ip.efficientChains, chain1)
-            addProxy = true
-        }
+    } else {
+        chains = append(chains, prefix)
     }
-    if addProxy {
-        ip.proxies = append(ip.proxies, proxy)
-    }
+    return chains
 }
 
-func (ep efficientProxies) isProxy(proxy string) (bool) {
-    if _, ok := ep[proxy]; ok {
-        return true
-    }
-    return false
+func (ep *efficientProxies) triggerAlert(prefix proxyChain, name string,
+                                         ie *inefficientProxies) {
+    ie.proxies = append(ie.proxies, name)
+    ie.efficientChains = ep.generateChains(prefix, name)
+    PrintAlert(ie)
 }
 
-func (ep *efficientProxies) set(proxy string, proxyPath proxyChain) {
-    (*ep)[proxy] = proxyChains{proxyPath}
-}
-
-func (ep *efficientProxies) add(proxy string, proxyPath proxyChain) {
-    (*ep)[proxy] = append((*ep)[proxy], proxyPath)
+type inefficientProxies struct {
+    originalChain proxyChain
+    proxies proxyChain
+    efficientChains proxyChains
 }
 
 func ProxyLoop(logs <-chan logyzer.LogEntry, done <-chan bool) {
@@ -79,50 +114,11 @@ func ProcessLog(entry logyzer.LogEntry, proxies *efficientProxies) {
         return
     }
     inProxies := entry.ProxyIPs()
-    var ip inefficientProxies
-    for i := len(inProxies) - 1; i >= 0; i-- {
-        proxy := inProxies[i]
-        var proxyPath proxyChain
-        if len(inProxies) > i {
-            proxyPath = inProxies[(i+1):]
-        }
-        if proxies.isProxy(proxy) {
-            Check(proxies, &ip, inProxies[:i+1], proxy, proxyPath)
-        } else {
-            proxies.set(proxy, proxyPath)
-        }
-    }
-    if len(ip.proxies) > 0 {
-        PrintAlert(inProxies, ip)
-    }
-    return
+    ip := inefficientProxies{originalChain: inProxies}
+    proxies.process(inProxies, 0, &ip)
 }
 
-func Check(ep *efficientProxies, ip *inefficientProxies,
-           prefixPath proxyChain, proxy string, proxyPath proxyChain) {
-    if len((*ep)[proxy][0]) > len(proxyPath) {
-        ep.set(proxy, proxyPath)
-    } else if len((*ep)[proxy][0]) == len(proxyPath) {
-        for _, pp := range (*ep)[proxy] {
-            if pp.equal(proxyPath) {
-                return
-            }
-        }
-        ep.add(proxy, proxyPath)
-    } else {
-        var options proxyChains
-        for _, chain := range (*ep)[proxy] {
-            option := make(proxyChain, len(prefixPath))
-            copy(option, prefixPath)
-            option = append(option, chain...)
-            options = append(options, option)
-        }
-        ip.add(proxy, options)
-    }
-}
-
-func FormatAlert(logProxyChain proxyChain,
-                 ip inefficientProxies) (string) {
+func FormatAlert(ip *inefficientProxies) (string) {
     timestamp := int(time.Now().Unix())
     j, _ := json.Marshal(&struct{
         Timestamp            int         `json:"timestamp"`
@@ -135,13 +131,13 @@ func FormatAlert(logProxyChain proxyChain,
         Timestamp: timestamp,
         MessageType: "alert",
         AlertType: "inefficient_proxy_chain",
-        ProxyChain: logProxyChain,
+        ProxyChain: ip.originalChain,
         InefficientAddresses: ip.proxies,
         EfficientProxyChains: ip.efficientChains,
     })
     return string(j)
 }
 
-func PrintAlert(logProxyChain proxyChain, ip inefficientProxies) {
-    fmt.Println(FormatAlert(logProxyChain, ip))
+func PrintAlert(ip *inefficientProxies) {
+    fmt.Println(FormatAlert(ip))
 }
